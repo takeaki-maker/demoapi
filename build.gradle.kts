d7dc4f53b9b572b1d0b199eee46deaa340314ff5
Hironosuke Watanabe <xxx@xxx.com>
2021-03-10 23:12:32 +0900

    dbのversionを8.0.22から8.0.23に修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation ("mysql:mysql-connector-java:8.0.22")
+	implementation ("mysql:mysql-connector-java:8.0.23")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
